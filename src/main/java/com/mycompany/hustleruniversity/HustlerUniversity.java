795f4e0fd95580b3053c2a48b1e3f9c94b7dee88
Maxim <Maxim@10.101.133.89>
2024-04-04 16:55:11 +0800

    Commit

---
+++ src/main/java/com/mycompany/hustleruniversity/HustlerUniversity.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hustleruniversity;
+
+/**
+ *
+ * @author Maxim
+ */
+public class HustlerUniversity {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
